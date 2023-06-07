3f6b8a3b5a5a2518d95c0a4d3d55f9fdfc6a5be3
André Tran <andre.tran@outscale.com>
2023-06-07 17:46:43 +0200

    DOC-4023: Enable Antora to access private repositories

---
+++ antora-extension/system-git-credential-manager.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const { spawn } = require('child_process')
+
+function callGitCredentialFill (url) {
+  const { protocol, host } = new URL(url)
+  return new Promise((resolve, reject) => {
+    const output = []
+    const process = spawn('git', ['credential', 'fill'])
+    process.on('close', (code) => {
+      if (code) return reject(code)
+      const { username, password } = output.join('\n').split('\n').reduce((acc, line) => {
+        if (line.startsWith('username') || line.startsWith('password')) {
+          const [key, val] = line.split('=')
+          acc[key] = val
+        }
+        return acc
+      }, {})
+      resolve(password ? { username, password } : username ? { token: username } : undefined)
+    })
+    process.stdout.on('data', (data) => output.push(data.toString().trim()))
+    process.stdin.write(`protocol=${protocol.slice(0, -1)}\nhost=${host}\n\n`)
+  })
+}
+
+module.exports = {
+  configure () {
+    this.urls = {}
+  },
+  async fill ({ url }) {
+    this.urls[url] = 'requested'
+    return callGitCredentialFill(url)
+  },
+  async approved ({ url }) {
+    this.urls[url] = 'approved'
+  },
+  async rejected ({ url, auth }) {
+    this.urls[url] = 'rejected'
+    const data = { statusCode: 401, statusMessage: 'HTTP Basic: Access Denied' }
+    const err = new Error(`HTTP Error: ${data.statusCode} ${data.statusMessage}`)
+    err.name = err.code = 'HttpError'
+    err.data = data
+    if (auth) err.rejected = true
+    throw err
+  },
+  status ({ url }) {
+    return this.urls[url]
+  },
+}
