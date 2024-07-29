b873a85946ccdd89e61c44d36fb1175970ffb525
André Tran <andre.tran@outscale.com>
2024-07-29 10:46:12 +0200

    Fix: DOC-4354: Make osc-doc-framework display a notice when update available

--- antora-extension/lib/update-checker.js
+++ antora-extension/lib/update-checker.js
@@ -14,7 +14,7 @@ async function checkUpdate () {
   const fetch = await git.fetch({ fs, http, onAuth, dir, singleBranch: true, ref, depth: 1, singleBranch: true })
   const remoteCommitId = fetch.fetchHead
 
-  if (localCommitId === remoteCommitId) {
+  if (localCommitId !== remoteCommitId) {
     console.log(
       '\u001b[33m\n'
       + 'Update available for osc-doc-framework!\n\n'
