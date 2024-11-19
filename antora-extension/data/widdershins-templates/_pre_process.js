b5488b0eef28fdea7639b5cacefd951c49498c8b
André Tran <andre.tran@outscale.com>
2024-11-19 17:12:57 +0100

    API DOCs: Don't omit the nested parameters of repeated objects

--- antora-extension/data/widdershins-templates/_pre_process.js
+++ antora-extension/data/widdershins-templates/_pre_process.js
@@ -1,3 +1,6 @@
+const jptr = require('reftools/lib/jptr.js').jptr
+const walkSchema = require('oas-schema-walker').walkSchema
+const wsGetState = require('oas-schema-walker').getDefaultState
 const generateCurlExamples = require('./code_shell')
 const generateHclExamples = require('./code_hcl')
 const generateJavaScriptExamples = require('./code_javascript')
@@ -18,6 +21,7 @@ function preProcess (data) {
   data.custom = {
     createTabs,
     evaluateResponses,
+    fakeBodyParameter,
     generateCurlExamples,
     generateHclExamples,
     generateJavaScriptExamples,
@@ -37,6 +41,7 @@ function preProcess (data) {
     printPattern,
     printRequired,
     printType,
+    schemaToArray,
     supportOperationMultipleExamples,
   }
 
@@ -201,6 +206,7 @@ function evaluateResponses (data) {
   }
 }
 
+// Modified from Widdershins
 function _getResponseExamples (data) {
   let content = ''
   const examples = []
@@ -278,6 +284,7 @@ function _getResponseExamples (data) {
   return content
 }
 
+// Modified from Widdershins
 function _convertExample (ex) {
   if (typeof ex === 'string') {
     try {
@@ -290,6 +297,7 @@ function _convertExample (ex) {
   }
 }
 
+// Modified from Widdershins
 function _doContentType (ctTypes, ctClass) {
   const contentTypes = {
     xml: ['^(application|text|image){1}\\/(.*\\+){0,1}xml(;){0,1}(\\s){0,}(charset=.*){0,}$'],
@@ -304,6 +312,51 @@ function _doContentType (ctTypes, ctClass) {
     }
   }
   return false
+}
+
+// Modified from Widdershins
+function fakeBodyParameter(data) {
+  if (!data.parameters) data.parameters = []
+  let bodyParams = []
+  if (data.bodyParameter.schema) {
+    let param = {}
+    param.in = 'body'
+    param.schema = data.bodyParameter.schema
+    param.name = 'body'
+    if (data.operation.requestBody) {
+      param.required = data.operation.requestBody.required || false
+      param.description = data.operation.requestBody.description
+      if (data.options.useBodyName && data.operation['x-body-name']) {
+        param.name = data.operation['x-body-name']
+      }
+    }
+    param.refName = data.bodyParameter.refName
+    if (!data.options.omitBody || param.schema["x-widdershins-oldRef"]) {
+      bodyParams.push(param)
+    }
+
+    if ((param.schema.type === 'object') && (data.options.expandBody || (!param.schema["x-widdershins-oldRef"]))) {
+      let offset = (data.options.omitBody ? -1 : 0)
+      let props = schemaToArray(data.bodyParameter.schema, offset, { trim: true }, data)
+
+      for (let block of props) {
+        for (let prop of block.rows) {
+          let param = {}
+          param.in = 'body'
+          param.schema = prop.schema
+          param.name = prop.displayName
+          param.required = prop.required
+          param.description = prop.description
+          param.safeType = prop.safeType
+          param.depth = prop.depth
+          bodyParams.push(param)
+        }
+      }
+    }
+
+    if (!data.parameters || !Array.isArray(data.parameters)) data.parameters = []
+    data.parameters = data.parameters.concat(bodyParams)
+  }
 }
 
 function getDeprecateState (description) {
@@ -328,6 +381,7 @@ function getIntroSecondPart (description) {
   }
 }
 
+// Modified from Widdershins
 function getOperationAuthenticationSchemes (data) {
   let list = ''
   for (const s in data.security) {
@@ -465,6 +519,249 @@ function printType (p) {
   s = s.replace('¦null', ' or null')
 
   return s
+}
+
+// Modified from Widdershins
+function schemaToArray(schema,offset,options,data) {
+  let iDepth = 0
+  let oDepth = 0
+  let blockDepth = 0
+  let skipDepth = -1
+  let container = []
+  let depthQueue = new Map()
+  let block = { title: '', rows: [] }
+  if (schema) {
+    if (schema.title) block.title = schema.title
+    if (!block.title && schema.description)
+      block.title = schema.description
+    block.description = schema.description
+    if (schema.externalDocs)
+      block.externalDocs = schema.externalDocs
+  }
+  container.push(block)
+  let wsState = wsGetState()
+  wsState.combine = true
+  wsState.allowRefSiblings = true
+  walkSchema(schema,{},wsState,function(schema,parent,state){
+    state.seen.delete(schema)
+
+    let isBlock = false
+    if (state.property && (state.property.startsWith('allOf') || state.property.startsWith('anyOf') || state.property.startsWith('oneOf') || (state.property === 'not'))) {
+      isBlock = true
+      let components = (state.property+'/0').split('/')
+      if (components[1] !== '0') {
+        if (components[0] === 'allOf') components[0] = 'and'
+        if (components[0] === 'anyOf') components[0] = 'or'
+        if (components[0] === 'oneOf') components[0] = 'xor'
+      }
+      block = { title: components[0], rows: [] }
+      let dschema = schema
+      let prefix = ''
+      if (schema.$ref) {
+        dschema = jptr(data.api,schema.$ref)
+        prefix = schema.$ref.replace('#/components/schemas/','')+'.'
+      }
+      if (dschema.discriminator) {
+        block.title += ' - discriminator: '+prefix+dschema.discriminator.propertyName
+      }
+      container.push(block)
+      blockDepth = state.depth
+    }
+    else {
+      if (blockDepth && state.depth < blockDepth) {
+        block = { title: data.translations.continued, rows: [] }
+        container.push(block)
+        blockDepth = 0
+      }
+    }
+
+    let entry = {}
+    entry.schema = schema
+    entry.in = 'body'
+    if (state.property && state.property.indexOf('/')) {
+      if (isBlock) entry.name = '*'+data.translations.anonymous+'*'
+      else entry.name = state.property.split('/')[1]
+    }
+    else if (!state.top) console.warn(state.property)
+    if (!entry.name && schema.title) entry.name = schema.title
+
+    if (schema.type === 'array' && schema.items && schema.items["x-widdershins-oldRef"] && !entry.name) {
+      state.top = false; // force it in
+    }
+    else if (schema.type === 'array' && schema.items && schema.items.$ref && !entry.name) {
+      state.top = false; // force it in, for un-dereferenced schemas
+    }
+    else if (!entry.name && state.top && schema.type && schema.type !== 'object' && schema.type !== 'array') {
+      state.top = false
+    }
+
+    if (!state.top && !entry.name && state.property === 'additionalProperties') {
+      entry.name = '**additionalProperties**'
+    }
+    if (!state.top && !entry.name && state.property === 'additionalItems') {
+      entry.name = '**additionalItems**'
+    }
+    if (!state.top && !entry.name && state.property && state.property.startsWith('patternProperties')) {
+      entry.name = '*'+entry.name+'*'
+    }
+    if (!state.top && !entry.name && !parent.items) {
+      entry.name = '*'+data.translations.anonymous+'*'
+    }
+
+    // we should be done futzing with entry.name now
+
+    if (entry.name) {
+      if (state.depth > iDepth) {
+        let difference = state.depth - iDepth
+        depthQueue.set(iDepth, difference)
+        oDepth++
+      }
+      if (state.depth < iDepth) {
+        let keys = depthQueue.keys()
+        let next = keys.next()
+        let difference = 0
+        while (!next.done) {
+          if (next.value >= state.depth) {
+            let depth = depthQueue.get(next.value)
+            depth = depth % 2 == 0 ? depth/2 : depth
+            difference += depth
+            depthQueue.delete(next.value)
+          }
+          next = keys.next()
+        }
+        oDepth -= difference
+        if (oDepth<0) oDepth=0
+      }
+      iDepth = state.depth
+      //console.warn('state %s, idepth %s, odepth now %s, offset %s',state.depth,iDepth,oDepth,offset)
+    }
+
+    entry.depth = Math.max(oDepth+offset,0)
+    entry.description = schema.description
+    entry.type = schema.type
+    entry.format = schema.format
+    entry.safeType = entry.type
+
+    if (schema["x-widdershins-oldRef"]) {
+      entry.$ref = schema["x-widdershins-oldRef"].replace('#/components/schemas/','')
+      entry.safeType = '['+entry.$ref+'](#schema'+entry.$ref.toLowerCase()+')'
+      if (data.options.shallowSchemas) skipDepth = entry.depth
+      if (!entry.description) {
+        let target = jptr(data.api,schema["x-widdershins-oldRef"])
+        if (target.description) entry.description = target.description
+      }
+    }
+    if (schema.$ref) { // repeat for un-dereferenced schemas
+      entry.$ref = schema.$ref.replace('#/components/schemas/','')
+      entry.type = '$ref'
+      entry.safeType = '['+entry.$ref+'](#schema'+entry.$ref.toLowerCase()+')'
+      if (data.options.shallowSchemas) skipDepth = entry.depth
+      if (!entry.description) {
+        let target = jptr(data.api,schema.$ref)
+        if (target.description) entry.description = target.description
+      }
+    }
+
+    if (entry.format) entry.safeType = entry.safeType+'('+entry.format+')'
+    if ((entry.type === 'array') && schema.items) {
+      let itemsType = schema.items.type||'any'
+      if (schema.items["x-widdershins-oldRef"]) {
+        let $ref = schema.items["x-widdershins-oldRef"].replace('#/components/schemas/','')
+        itemsType = '['+$ref+'](#schema'+$ref.toLowerCase()+')'
+        if (!entry.description) {
+          let target = jptr(data.api,schema.items["x-widdershins-oldRef"])
+          if (target.description) entry.description = '['+target.description+']'
+        }
+      }
+      if (schema.items.$ref) { // repeat for un-dereferenced schemas
+        let $ref = schema.items.$ref.replace('#/components/schemas/','')
+        itemsType = '['+$ref+'](#schema'+$ref.toLowerCase()+')'
+        if (!entry.description) {
+          let target = jptr(data.api,schema.items.$ref)
+          if (target.description) entry.description = '['+target.description+']'
+        }
+      }
+      if (schema.items.anyOf) itemsType = 'anyOf'
+      if (schema.items.allOf) itemsType = 'allOf'
+      if (schema.items.oneOf) itemsType = 'oneOf'
+      if (schema.items.not) itemsType = 'not'
+      entry.safeType = '['+itemsType+']'
+    }
+
+    if (options.trim && typeof entry.description === 'string') {
+      entry.description = entry.description.trim()
+    }
+    if (options.join && typeof entry.description === 'string') {
+      entry.description = entry.description.split('\r').join('').split('\n').join('<br>')
+    }
+    if (options.truncate && typeof entry.description === 'string') {
+      entry.description = entry.description.split('\r').join('').split('\n')[0]
+    }
+    if (entry.description === 'undefined') { // yes, the string
+      entry.description = ''
+    }
+
+    if (schema.nullable === true) {
+      entry.safeType += '¦null'
+    }
+
+    if (schema.readOnly) entry.restrictions = data.translations.readOnly
+    if (schema.writeOnly) entry.restrictions = data.translations.writeOnly
+
+    entry.required = (parent.required && Array.isArray(parent.required) && parent.required.indexOf(entry.name)>=0)
+    if (typeof entry.required === 'undefined') entry.required = false
+
+    if (typeof entry.type === 'undefined') {
+      entry.type = _inferType(schema)
+      entry.safeType = entry.type
+    }
+
+    if (typeof entry.name === 'string' && entry.name.startsWith('x-widdershins-')) {
+      entry.name = ''; // reset
+    }
+    if ((skipDepth >= 0) && (entry.depth >= skipDepth)) entry.name = ''; // reset
+    if (entry.depth < skipDepth) skipDepth = -1
+    entry.displayName = (data.translations.indent.repeat(entry.depth)+' '+entry.name).trim()
+
+    if ((!state.top || entry.type !== 'object') && (entry.name)) {
+      block.rows.push(entry)
+    }
+  })
+  return container
+}
+
+// Modified from Widdershins
+function _inferType(schema) {
+
+  function has(properties) {
+    for (let property of properties) {
+      if (typeof schema[property] !== 'undefined') return true
+    }
+    return false
+  }
+
+  if (schema.type) return schema.type
+  let possibleTypes = []
+  if (has(['properties','additionalProperties','patternProperties','minProperties','maxProperties','required','dependencies'])) {
+    possibleTypes.push('object')
+  }
+  if (has(['items','additionalItems','maxItems','minItems','uniqueItems'])) {
+    possibleTypes.push('array')
+  }
+  if (has(['exclusiveMaximum','exclusiveMinimum','maximum','minimum','multipleOf'])) {
+    possibleTypes.push('number')
+  }
+  if (has(['maxLength','minLength','pattern'])) {
+    possibleTypes.push('number')
+  }
+  if (schema.enum) {
+    for (let value of schema.enum) {
+      possibleTypes.push(typeof value); // doesn't matter about dupes
+    }
+  }
+
+  if (possibleTypes.length === 1) return possibleTypes[0]
+  return 'any'
 }
 
 function supportOperationMultipleExamples (data) {
